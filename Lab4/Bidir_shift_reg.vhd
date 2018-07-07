e8b973f4048e80b322be07660adbd579369e2267
mattfung99 <official.matthiasfung@gmail.com>
2018-07-07 12:10:52 -0400

    Update AGAIN

--- Lab4/Bidir_shift_reg.vhd
+++ Lab4/Bidir_shift_reg.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 Entity Bidir_shift_reg is port(
-	CLK				: in std_logic := '0';
+	CLK				: in std_logic := '0';					
 	RESET_n			: in std_logic := '0';
 	CLK_EN			: in std_logic := '0';
 	LEFT0_RIGHT1	: in std_logic := '0';
@@ -21,10 +21,10 @@ begin
 		if(RESET_n = '0') then
 				sreg <= "0000";
 		elsif(rising_edge(CLK) AND (CLK_EN = '1')) then
-				if(LEFT0_RIGHT1 = '1') then  -- TRUE for right shift
-						sreg(3 downto 0) <= '1' & sreg(3 downto 1);  -- right shift of bits
+				if(LEFT0_RIGHT1 = '1') then  							-- TRUE for right shift
+						sreg(3 downto 0) <= '1' & sreg(3 downto 1);  	-- Right shift of bits
 				elsif(LEFT0_RIGHT1 = '0') then
-						sreg(3 downto 0) <= sreg(2 downto 0) & '0';  -- left shift of bits
+						sreg(3 downto 0) <= sreg(2 downto 0) & '0';  	-- Left shift of bits
 				end if;
 		end if;
 		REG_BITS <= sreg;
